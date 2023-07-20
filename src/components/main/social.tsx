9ab488f7415468ca686bc0ea1ebb4e635195e7a8
Paulo Henrique <phttm0501@gmail.com>
2023-07-20 15:47:04 -0400

    social att

--- src/components/main/social.tsx
+++ src/components/main/social.tsx
@@ -4,13 +4,15 @@ import Whats from "public/icons8-whatsapp.svg";
 export default function Social() {
   return (
     <div className="flex gap-1 mt-3 justify-center">
-      <a href="https://www.instagram.com/paulo_henrique_ms_/">
+      <a
+        href="https://www.linkedin.com/in/paulo-henrique-001b84243/"
+        target="_blank"
+      >
         <Linkedin className="text-cyan-950 hover:text-cyan-700 cursor-pointer" />
       </a>
-      <a href="https://github.com/PauloHenriqueM-S">
+      <a href="https://github.com/PauloHenriqueM-S" target="_blank">
         <Github className="text-cyan-950 hover:text-cyan-700 cursor-pointer" />
       </a>
-      <a href="https://github.com/PauloHenriqueM-S"></a>
     </div>
   );
 }
